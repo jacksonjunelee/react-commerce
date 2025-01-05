d75b7c4b191f10fe72ab43cc1f8d0531af1084ec
jacksonjunelee <jjlee5266@gmail.com>
2025-01-04 23:07:44 -0500

    added back mock data

--- src/state/actions/productActions.js
+++ src/state/actions/productActions.js
@@ -1,36 +1,37 @@
-import axios from 'axios';
-import { MOCK_DATA } from '../mockData';
+import axios from "axios";
+import { MOCK_DATA } from "../mockData";
 
-const API_URL = 'http://localhost:5000/api/products'; // Adjust this URL as necessary
+const API_URL = "http://localhost:5000/api/products"; // Adjust this URL as necessary
 
 export const fetchProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: 'FETCH_PRODUCTS_REQUEST' });
+    dispatch({ type: "FETCH_PRODUCTS_REQUEST" });
 
     // const { data } = await axios.get('/api/products'); // Use your API
 
-    // const mockApi = new Promise((resolve) => {
-    //   setTimeout(() => {
-    //     resolve({
-    //       data: MOCK_DATA
-    //     });
-    //   }, 1000)
-    // })
+    const mockApi = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          data: MOCK_DATA,
+        });
+      }, 1000);
+    });
 
-    // const { data } = await mockApi;
-    const response = await axios.get(API_URL);
-    const data = response.data; // Return the product data
+    const { data } = await mockApi;
+    // const response = await axios.get(API_URL);
+    // const data = response.data; // Return the product data
 
     dispatch({
-      type: 'FETCH_PRODUCTS_SUCCESS',
+      type: "FETCH_PRODUCTS_SUCCESS",
       payload: data,
     });
   } catch (error) {
     dispatch({
-      type: 'FETCH_PRODUCTS_FAILURE',
-      payload: error.response && error.response.data.message
-        ? error.response.data.message
-        : error.message,
+      type: "FETCH_PRODUCTS_FAILURE",
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
     });
   }
 };
